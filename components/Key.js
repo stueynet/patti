05daa2bdaf829f13af8e73d147cfd12642d50f90
Stuart Starr <stuart@stueynet.com>
2019-02-24 14:12:16 -0500

    use sortable grid ftw

--- components/Key.js
+++ components/Key.js
@@ -5,56 +5,57 @@ import { Icon } from 'expo';
 import Colors from '../constants/Colors';
 
 export default class Key extends React.Component {
-  render() {
+	render() {
+		const fontSize = this.getFontSize();
 
-      const fontSize = this.getFontSize();
-      console.log(fontSize);
+		return (
+			<TouchableOpacity
+                disabled={! this.props.disabled}
+				style={[ styles.container, { backgroundColor: this.props.colour } ]}
+				onPress={this.props.handleKeyPress}
+				onLongPress={this.props.handleLongPress}
+			>
+                <Text style={[ styles.default, { fontSize } ]}>{this.props.label}</Text>
+			</TouchableOpacity>
+		);
+	}
 
-      const editMode = this.props.editing ? {border}
-
-    return (
-    <TouchableOpacity 
-        style={[styles.container, {backgroundColor: this.props.colour}]} 
-        onPress={this.props.handleKeyPress}
-        onLongPress={this.props.handleLongPress}
-    >
-         <Text style={[styles.default, {fontSize}]}>{this.props.label}</Text>
-    </TouchableOpacity>
-    );
-  }
-
-  getFontSize(){
-        size = 30 * (1/(this.props.label.length / 5));
-        console.log(size);
-        return size > 64 ? 64 : size;   
-    }
+	getFontSize() {
+		size = 30 * (1 / (this.props.label.length / 5));
+		return size > 64 ? 64 : size;
+	}
 }
 Key.defaultProps = {
-  text: "???",
-  colour: "blue",
-  type: "default",
+	text: '???',
+	colour: 'blue',
+	type: 'default'
 };
 
 const styles = StyleSheet.create({
-  container: {
-        alignItems: 'center',
-        justifyContent: 'center',
-        aspectRatio: 1,
-        width: 128,
-        height: 120,
-        margin: 10,
-    },
-    textBase: {
-        color: 'white',
-    },
-    default: {
-        color: 'white',
-        fontSize: 60,
-    },
-    word: {
-        fontSize: 30,
-    },
-    letter: {
-        fontSize: 60,
-    }
+	container: {
+         flex: 1,
+            margin: 8,
+            borderRadius: 20,
+            justifyContent: 'center',
+            alignItems: 'center'
+		// alignItems: 'center',
+		// justifyContent: 'center',
+		// aspectRatio: 1,
+		// width: 128,
+		// height: 120,
+		// margin: 10
+	},
+	textBase: {
+		color: 'white'
+	},
+	default: {
+		color: 'white',
+		fontSize: 60
+	},
+	word: {
+		fontSize: 30
+	},
+	letter: {
+		fontSize: 60
+	}
 });
--- screens/KeyboardScreen.js
+++ screens/KeyboardScreen.js
@@ -1,203 +1,216 @@
 import React from 'react';
-import {
-  Image,
-  Platform,
-  ScrollView,
-  StyleSheet,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
+import { Image, Platform, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import { Icon } from 'expo';
+
+import SortableGrid from 'react-native-sortable-grid';
 
 import { MonoText } from '../components/StyledText';
 import Key from '../components/Key';
 import Section from '../components/Section';
 
 const newButton = {
-  label: "new",
-  value: "new",
-  colour: 'pink',
-}
+	label: 'new',
+	value: 'new',
+	colour: 'pink'
+};
 
 export default class KeyboardScreen extends React.Component {
-  static navigationOptions = {
-    header: null,
-  };
+	static navigationOptions = {
+		header: null
+	};
 
-  constructor(props) {
-    super(props);
-    this.state = { 
-      editMode: false,
-      text: "",
-      buttons: [
-        {
-          id: 1,
-          label: "a",
-          value: "a",
-        },
-        {
-          id: 2,
-          label: "More stuff",
-          value: "b",
-          spacer: true,
-        },
-        {
-          id: 3,
-          label: "Koby",
-          value: "Koby",
-        },
-        {
-          id: 4,
-          label: "Max",
-          value: "Max",
-        }
-      ],
-    };
+	constructor(props) {
+		super(props);
+		this.state = {
+			editMode: false,
+			text: '',
+			buttons: [
+				'a',
+				'b',
+				'c',
+				'd',
+				'e',
+				'f',
+				'g',
+				'h',
+				'i',
+				'j',
+				'k',
+				'l',
+				'm',
+				'n',
+				'o',
+				'p',
+				'q',
+				'r',
+				's',
+				't',
+				'u',
+				'v',
+				'w',
+				'x',
+				'y',
+				'z'
+			]
+		};
+	}
+
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.textWrapper}>
+					<View style={styles.columnsWrapper}>
+						<Text style={styles.text}>{this.state.text}</Text>
+						<View style={styles.clearWrapper}>
+							<Icon.Ionicons
+								name="ios-remove-circle-outline"
+								size={72}
+								onPress={this.handleBack}
+								style={{ marginRight: 10 }}
+							/>
+							<Icon.Ionicons name="ios-close-circle-outline" size={72} onPress={this.handleClear} />
+						</View>
+					</View>
+				</View>
+        {this.renderEditMode()}
+				<ScrollView scrollEnabled={! this.state.editMode} contentContainerStyle={styles.contentContainer}>
+					{this.renderButtons()}
+				</ScrollView>
+				<View style={styles.newButton}>
+					<Icon.Ionicons
+						name="ios-add-circle-outline"
+						size={72}
+						onPress={() => this.handleAddButton(newButton)}
+					/>
+				</View>
+			</View>
+		);
+	}
+
+	renderButtons() {
+		const buttons = this.state.buttons.map((button, index) => {
+			return this.renderButton(button, index);
+		});
+		return (
+			<SortableGrid
+				blockTransitionDuration={400}
+				activeBlockCenteringDuration={200}
+				itemsPerRow={4}
+				dragActivationTreshold={200}
+				onDragRelease={this.handleDragRelease}
+				onDragStart={this.handleDragStart}
+        onPress={this.handleKeyPress}
+			>
+				{buttons}
+			</SortableGrid>
+		);
+	}
+
+  handleDragStart = (itemOrder) => {
+		console.log('Some block is being dragged now!');
+    this.setState({editMode: true});
   }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.textWrapper}>
-          <View style={styles.columnsWrapper}>
-            <Text style={styles.text}>{this.state.text}</Text>
-            <View style={styles.clearWrapper}>
-             <Icon.Ionicons
-                name="ios-remove-circle-outline"
-                size={72}
-                onPress={this.handleBack}
-                style={{marginRight: 10}}
-              />
-              <Icon.Ionicons
-                name="ios-close-circle-outline"
-                size={72}
-                onPress={this.handleClear}
-              />             
-            </View>
-          </View>
-        </View>
-        <ScrollView contentContainerStyle={styles.contentContainer}>
-          {this.renderButtons()}
-          {this.renderEditMode()}
-        </ScrollView>
-        <View style={styles.newButton}>
-            <Icon.Ionicons
-                  name="ios-add-circle-outline"
-                  size={72}
-                  onPress={() => this.handleAddButton(newButton)}
-            />   
-          </View>
-      </View>
-    );
+  handleDragRelease = (itemOrder) => {
+    this.setState({editMode: false});
+		console.log('Drag was released, the blocks are in the following order: ', itemOrder);
   }
 
-  renderButtons() {
-    const buttons = this.state.buttons.map(button => {
-       return this.renderButton(button);
-    });
-    return (
-      <Section>
-      {buttons}
-      </Section>
-    )
-  }
+	renderEditMode = () => {
+		if (!this.state.editMode) {
+			return null;
+		}
 
-  renderEditMode = () => {
-    if (! this.state.editMode) {
-      return null;
-    }
+		return (
+			<View style={styles.editMode}>
+				<Text>You are currently editing the button layout</Text>
+				<Icon.Ionicons name="ios-close-circle-outline" size={32} onPress={this.handleDisableEditMode} />
+			</View>
+		);
+	};
 
-    return (
-      <View style={styles.editMode}>
-        <Text>You are currently editing the button layout</Text>
-        <Icon.Ionicons
-          name="ios-close-circle-outline"
-          size={32}
-          onPress={this.handleDisableEditMode}
-        />     
-      </View>
-    )
-  };
+	handleKeyPress = (text) => {
+		return this.setState({ text: this.state.text + ' ' + text });
+	};
 
-  handleKeyPress = (text) => {
-    return this.setState({text: this.state.text + " " + text});
-  }
+	handleLongPress = () => {
+		return this.setState((prevState) => ({
+			editMode: !prevState.editMode
+		}));
+	};
 
-  handleLongPress = () => {
-    return this.setState(prevState => ({
-      editMode: !prevState.editMode,
-    }));
-  }
+	handleDisableEditMode = () => {
+		return this.setState((prevState) => ({
+			editMode: false
+		}));
+	};
 
-  handleDisableEditMode = () => {
-    return this.setState(prevState => ({
-      editMode: false,
-    }));
-  }
+	handleBack = () => {
+		const theString = this.state.text;
+		const newText = theString.slice(0, -1);
+		return this.setState((prevState) => ({ text: newText }));
+	};
 
-  handleBack = () => {
-    const theString = this.state.text;
-    const newText = theString.slice(0, -1);
-    return this.setState(prevState => ({text: newText}));
-  }
+	handleClear = () => {
+		return this.setState((prevState) => ({ text: '' }));
+	};
 
-  handleClear = () => {
-    return this.setState(prevState => ({text: ""}));
-  }
+	renderButton = (button, index) => {
+    console.log(this.state.editMode);
+		return (
+			<Key
+				key={index}
+				label={button}
+        disabled={this.state.editMode}
+				handleKeyPress={() => this.handleKeyPress(button)}
+				handleLongPress={this.handleLongPress}
+			/>
+		);
+	};
 
-  renderButton = (button) => {
-    if(button.spacer) {
-      return <Text key={button.id}>{button.label}</Text>
-    }
-
-    return <Key key={button.value} label={button.label} handleKeyPress={() => this.handleKeyPress(button.value)} handleLongPress = {this.handleLongPress} colour={button.colour} />;
-  } 
-
-  handleAddButton = (button) => {
-    const sectionToUpdate = this.state.sections.find(section => section.id === sectionId);
-    const sections = {...this.state.sections};
-    sectionToUpdate.buttons.push(button);
-    this.setState({});
-    console.log(sectionToUpdate.buttons);
-  }
+	handleAddButton = (button) => {
+		const sectionToUpdate = this.state.sections.find((section) => section.id === sectionId);
+		const sections = { ...this.state.sections };
+		sectionToUpdate.buttons.push(button);
+	};
 }
 
 const styles = StyleSheet.create({
-  container: {
-    paddingTop: 30,
-    backgroundColor: '#fff',
-    flex: 1,
-  },
-  textWrapper: {
-    backgroundColor: '#EFEFEF',
-    justifyContent: 'center',
-    padding: 20,
-    minHeight: 100,
-  },
-  columnsWrapper: {
-    flexDirection: 'row',
-  },
-  clearWrapper: {
-    alignSelf: 'flex-end',
-    flexDirection: 'row',
-  },
-  text: {
-    fontSize: 30,
-    flex: 1,
-  },
-  contentContainer: {
-    padding: 10,
-  },
-  newButton: {
-    position: 'absolute',
-    right: 10,
-    bottom: 10,
-  },
-  editMode: {
-    padding: 20,
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  }
+	container: {
+		paddingTop: 30,
+		backgroundColor: '#fff',
+		flex: 1
+	},
+	textWrapper: {
+		backgroundColor: '#EFEFEF',
+		justifyContent: 'center',
+		padding: 20,
+		minHeight: 100
+	},
+	columnsWrapper: {
+		flexDirection: 'row'
+	},
+	clearWrapper: {
+		alignSelf: 'flex-end',
+		flexDirection: 'row'
+	},
+	text: {
+		fontSize: 30,
+		flex: 1
+	},
+	contentContainer: {
+		padding: 10
+	},
+	newButton: {
+		position: 'absolute',
+		right: 10,
+		bottom: 10
+	},
+	editMode: {
+		padding: 20,
+    backgroundColor: 'blue',
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between'
+	}
 });
